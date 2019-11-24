96f85210a4f446151999880dacf1f932d1ebedea
sukolupo <git@sukolupo.com>
2019-11-24 19:45:31 +0000

    tweaks to line ednoings

--- sli-metrics/modules/exporter.py
+++ sli-metrics/modules/exporter.py
@@ -37,12 +37,12 @@ def process_metrics(app, metric_dir="./metrics", pattern=".metric"):
 
 
 def add_description(m_name,  m_desc, m_type, m_comment):
-    template = "# HELP {0} {1}</br># TYPE {0} {2}</br># {3}</br>"
+    template = "# HELP {0} {1}\r\n# TYPE {0} {2}\r\n# {3}\r\n"
 
     return template.format(m_name, m_desc, m_type, m_comment)
 
 
 def add_metric(m_name, m_label, m_value):
-    template="{0}{{{1}}} {2}</br>"
+    template="{0}{{{1}}} {2}\r\n"
     return template.format(m_name, m_label, m_value)
 
